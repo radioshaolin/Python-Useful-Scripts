8e99aa16b7436384996061fa78ff8162af94d859
Ivan Zemlyaniy <shaolinfm@gmail.com>
2015-06-03 09:42:20 +0300

    Cosmetic changes for my particularly system
    Little update of command, added sudo and -H attributem just needed for
    my system.

--- python_all_packs_update.py
+++ python_all_packs_update.py
@@ -1,5 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Ivan Zemlyaniy aka shaolinfm
+# @Date:   2015-04-27 16:46:53
+# @Last Modified by:   shaolinfm
+# @Last Modified time: 2015-06-03 09:38:55
+
+"""
+This is simmple script for updating all Python distributives in System
+"""
+
 import pip
 from subprocess import call
 
-for dist in pip.get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+def python_all_packs_update():
+    """Function for automatical update of all Python distributives installed in System"""
+
+    for dist in pip.get_installed_distributions():
+        call("sudo -H pip install --upgrade " + dist.project_name, shell=True)
+
+if __name__ == '__main__':
+    python_all_packs_update()
